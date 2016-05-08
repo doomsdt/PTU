867a212cff744da489448bed567e78bc63225310
doomsdt <doomsdt@naver.com>
2016-05-08 13:20:05 +0900

    change dbURI

--- db.js
+++ db.js
@@ -1,18 +1,18 @@
 var mongoose = require('mongoose');
 
-var dbURI = 'mongodb://localhost/test';
+var dbURI = 'mongodb://admin:5891421l;@ds017432.mlab.com:17432/heroku_2k97k4d2';
 
 exports.connect = function(){
-	
+
 	mongoose.connect(dbURI);
 	mongoose.connection.on('connected', function(){
 		console.log('Succeed to get connection pool' + dbURI);
 	});
-	
+
 	mongoose.connection.on('error', function(err){
 		console.log('Failed to get connection in mongoose, err is ' + err);
 	});
-	
+
 	mongoose.connection.on('disconnected', function(){
 		console.log('Database connection has disconnected.');
 	});
