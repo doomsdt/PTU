b532fd29bd8fa5d90c4bccc6fe6ec9693dbdc365
Jinmoon Lee <doomsdt@gmail.com>
2016-05-08 15:53:57 +0900

    FB login test

--- router.js
+++ router.js
@@ -1,6 +1,10 @@
 var task = require('./controllers/task-controller.js');
 
 exports.route = function(app){
+	app.get('/',function(req,res){
+		res.redirect('/cal.html');
+		res.end();
+	})
 	app.post('/list', task.list);
 	app.post('/createTask', task.create);
 	//app.post('/updateTask', task.update);
