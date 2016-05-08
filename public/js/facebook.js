bd60afdb2f0b75214c45ecc2a5fc5b15ebb752ec
Jinmoon Lee <doomsdt@gmail.com>
2016-05-08 17:00:03 +0900

    FB login test

--- public/js/facebook.js
+++ public/js/facebook.js
@@ -31,24 +31,13 @@ function checkLoginState() {
   });
 }
 
-	
-
-	// Load the SDK asynchronously
-	(function(d, s, id) {
-	  var js, fjs = d.getElementsByTagName(s)[0];
-	  if (d.getElementById(id)) return;
-	  js = d.createElement(s); js.id = id;
-	  js.src = "//connect.facebook.net/en_US/sdk.js";
-	  fjs.parentNode.insertBefore(js, fjs);
-	}(document, 'script', 'facebook-jssdk'));
-
-	// Here we run a very simple test of the Graph API after login is
-	// successful.  See statusChangeCallback() for when this call is made.
-	function testAPI() {
-	  console.log('Welcome!  Fetching your information.... ');
-	  FB.api('/me?fields=name,picture,friends', function(response) {
-	    console.log(JSON.stringify(response));
-	    document.getElementById('status').innerHTML =
-	      'Thanks for logging in, ' + response.name + '!';
-	  });
-	}
+// Here we run a very simple test of the Graph API after login is
+// successful.  See statusChangeCallback() for when this call is made.
+function testAPI() {
+  console.log('Welcome!  Fetching your information.... ');
+  FB.api('/me?fields=name,picture,friends', function(response) {
+    console.log(JSON.stringify(response));
+    document.getElementById('status').innerHTML =
+      'Thanks for logging in, ' + response.name + '!';
+  });
+}
