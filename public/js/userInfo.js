0d5fea4e67cf988a04847292f9d9941ee2e551b0
Jinmoon Lee <doomsdt@gmail.com>
2016-05-18 13:49:58 +0900

    facebook test

---
+++ public/js/userInfo.js
@@ -0,0 +1,9 @@
+
+function setLogout(){
+	$('#logoutButton input').on('click',function(){
+		FB.logout(function(response){
+			$('#logoutButton').hide();
+			$('#loginButton').show();
+		});
+	});
+}
