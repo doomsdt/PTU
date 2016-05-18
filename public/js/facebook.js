c2a791fbb589f7b35e0d28bf4642135259d9d6ec
Jinmoon Lee <doomsdt@gmail.com>
2016-05-18 14:18:40 +0900

    facebook test

--- public/js/facebook.js
+++ public/js/facebook.js
@@ -18,15 +18,15 @@ function statusChangeCallback(response) {
   	    setUserInfo();
   	});
 
-  } else if (response.status === 'not_authorized') {
-    // The person is logged into Facebook, but not your app.
-    //document.getElementById('status').innerHTML = 'Please log ' +
-    //  'into this app.';
   } else {
     // The person is not logged into Facebook, so we're not sure if
     // they are logged into this app or not.
     //document.getElementById('status').innerHTML = 'Please log ' +
     //  'into Facebook.';
+	  $('#userId').attr('value','');
+	    $('#userName').attr('value','');
+	    $('#picUrl').attr('value','');
+	    setUserInfo();
   }
 }
 
