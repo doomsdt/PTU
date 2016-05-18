6d41afbc4dee024eaa8fcc22b7ad766605781bcb
Jinmoon Lee <doomsdt@gmail.com>
2016-05-18 15:57:37 +0900

    facebook test

--- public/js/alert.js
+++ public/js/alert.js
@@ -1,5 +1,5 @@
 function alertSuccess(message){
-	$('.alert-success span').val(message);
+	$('.alert-success span').text(message);
 	$('.alert-success').show();
 	setTimeout(function() {
 	        $(".alert-success").hide();
