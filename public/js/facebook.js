b98bf58dabcd36c8cd8a5b11be6333d544465a08
Jinmoon Lee <doomsdt@gmail.com>
2016-05-18 14:05:58 +0900

    facebook test

--- public/js/facebook.js
+++ public/js/facebook.js
@@ -1,15 +1,21 @@
 
 // This is called with the results from from FB.getLoginStatus().
 function statusChangeCallback(response) {
-  console.log('statusChangeCallback');
-  console.log(response);
   // The response object is returned with a status field that lets the
   // app know the current login status of the person.
   // Full docs on the response object can be found in the documentation
   // for FB.getLoginStatus().
   if (response.status === 'connected') {
     // Logged into your app and Facebook.
-    testAPI();
+	$('#loginButton').hide();
+  	$('#logoutButton').show();
+  	FB.api('/me?fields=name,picture,friends', function(res) {
+  		console.log(res);
+  	    $('#userId').attr('value',res.id);
+  	    $('#userName').attr('value',res.name);
+  	    $('#picUrl').attr('value',res.picture.url);
+  	});
+
   } else if (response.status === 'not_authorized') {
     // The person is logged into Facebook, but not your app.
     //document.getElementById('status').innerHTML = 'Please log ' +
